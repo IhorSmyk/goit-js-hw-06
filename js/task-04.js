e191d0dc8f70fd372d93756b508a152bd953ca20
Ihor Smyk <ismyk256@gmail.com>
2022-11-12 00:06:43 +0200

    a

--- js/task-04.js
+++ js/task-04.js
@@ -12,3 +12,12 @@
     Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
     Оновлюй інтерфейс новим значенням змінної counterValue.
      */
+
+let value = 0;
+
+const counterRef = document.querySelector("#value")
+const decrement = document.querySelector('[data-action ="decrement"]')
+const increment = document.querySelector('[data-action ="increment"]')
+
+increment.addEventListener("click", () => counterRef.textContent = ++value);
+decrement.addEventListener("click", () => counterRef.textContent = --value);
