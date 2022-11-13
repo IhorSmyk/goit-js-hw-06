204cb8d35594e37c90fd13884915082434bd5811
Ihor Smyk <ismyk256@gmail.com>
2022-11-13 17:24:35 +0200

    ready

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    if (formData.get("email") === "" || formData.get("password") === "") {
+        return alert("not all fields are filled!");
+    } else {
+        const newUser = {
+            email: formData.get("email"),
+            password: formData.get("password"),
+        };
+        event.currentTarget.reset();
+        console.log(newUser);
+        return newUser;
+    }
+}
+
+
