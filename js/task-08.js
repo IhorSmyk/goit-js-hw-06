c426bec3c2ad8238a56dd7a2d3f44dbe5663b6c2
Ihor Smyk <ismyk256@gmail.com>
2022-11-20 20:48:46 +0200

    some change

--- js/task-08.js
+++ js/task-08.js
@@ -1,22 +1,20 @@
+const form = document.querySelector('.login-form');
 
-const form = document.querySelector(".login-form");
-
-form.addEventListener("submit", onFormSubmit);
+form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    if (formData.get("email") === "" || formData.get("password") === "") {
-        return alert("not all fields are filled!");
-    } else {
-        const newUser = {
-            email: formData.get("email"),
-            password: formData.get("password"),
-        };
-        event.currentTarget.reset();
-        console.log(newUser);
-        return newUser;
-    }
+  event.preventDefault();
+  const formData = new FormData(event.currentTarget);
+  if (formData.get('email') === '' || formData.get('password') === '') {
+    return alert('not all fields are filled!');
+  } else {
+    const newUser = {
+      email: formData.get('email'),
+      password: formData.get('password'),
+    };
+    event.currentTarget.reset();
+    console.log(newUser);
+    console.log(formData);
+    return newUser;
+  }
 }
-
-
